2f45edb51f0d169a7c7f570c05a8900382783a53
Glen Adams <glen.adams@gmail.com>
2024-09-08 11:34:13 +0200

    add the distribution tars

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='quickstats',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Common statistics utility functions',
     long_description=open('README.md').read(),
     install_requires=['numpy', 'scipy'],
-    url='https://github.com/<username>/<package-name>',
+    url='https://github.com/glenad/quick-stats.git',
     author='Glen Adams',
     author_email='glen.adams@gmail.com'
 )
